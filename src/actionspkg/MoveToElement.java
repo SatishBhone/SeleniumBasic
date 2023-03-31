4b7fc60a96e937ff71f338f022972609c110f125
Satish Bhone <satishbhone@gmail.com>
2023-03-31 12:33:50 +0530

    add movi to element code

---
+++ src/actionspkg/MoveToElement.java
@@ -0,0 +1,43 @@
+package actionspkg;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class MoveToElement {
+	//1)moveToElement(): 
+	//	If we want to move mouse pointer from one position to another position that time we use 
+	//	method moveToElement().
+	public static void main(String[] args) throws InterruptedException 
+	{
+
+	       System.setProperty("webdriver.chrome.driver", "C:\\Users\\satis\\Driver\\chromedriver_win32\\chromedriver.exe");
+	     	
+	     	ChromeOptions options = new ChromeOptions();
+			options.addArguments("--remote-allow-origins=*");
+			options.addArguments("--disable notifications");
+			DesiredCapabilities cp = new DesiredCapabilities();
+			cp.setCapability(ChromeOptions.CAPABILITY, options);
+			options.merge(cp);
+	     	
+			WebDriver driver = new ChromeDriver(options);
+			driver.get("https://www.google.co.in/");
+			
+			driver.manage().window().maximize();
+			
+			Thread.sleep(2000);
+		
+			WebElement gmail = driver.findElement(By.linkText("Gmail"));
+			
+			Actions act = new Actions(driver);
+			
+			act.moveToElement(gmail).click().perform();
+		
+		
+	}
+
+}
