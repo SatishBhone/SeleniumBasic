ff27dfdaf56e37f82d31dfe1919e401fe6a2a3c1
Satish Bhone <satishbhone@gmail.com>
2023-04-02 14:59:41 +0530

    facebook signUp code added

--- src/actionspkg/TabKeyAction.java
+++ src/actionspkg/TabKeyAction.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 public class TabKeyAction {
 
@@ -29,10 +30,13 @@ public class TabKeyAction {
 		
 		driver.get("https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C589460569849%7Cb%7Cfacebook%20%27%7C&placement=&creative=589460569849&keyword=facebook%20%27&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696220912%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-327195741349%26loc_physical_ms%3D9300351%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=EAIaIQobChMIt4PWnpyI_gIV0TUrCh1qRwgiEAAYASAAEgLhDvD_BwE");
 		
+		driver.manage().window().maximize();
+		
 		WebElement firstname = driver.findElement(By.xpath("(//input[@type='text'])[1]"));
 		
 		firstname.sendKeys("Satish");
 		Thread.sleep(2000);
+		
 		Actions action = new Actions(driver);
 		
 		action.sendKeys(Keys.TAB).build().perform();
@@ -53,13 +57,54 @@ public class TabKeyAction {
 		Thread.sleep(2000);
 		action.sendKeys(Keys.TAB).perform();
 		Thread.sleep(2000);
-		List<WebElement> list = driver.findElements(By.xpath("//select[@id='day']"));
-		Thread.sleep(2000);
 		
-		
-		
-		
-
+	     driver.findElement(By.xpath("//select[@id='day']")).click();
+	 	Thread.sleep(2000);
+	     
+	     for(int i=14; i>0; i--) 
+	     {
+	    	 action.sendKeys(Keys.ARROW_DOWN).build().perform();
+	     }
+	     
+    	 action.click().perform();
+    	 Thread.sleep(2000);
+    	 action.sendKeys(Keys.TAB).build().perform();
+    	 
+    	 driver.findElement(By.xpath("//select[@id='month']")).click();
+    	 Thread.sleep(2000);
+    	 
+    	 for(int i=2; i>0; i--) 
+    	 {
+    		 action.sendKeys(Keys.ARROW_UP).build().perform();
+    		 Thread.sleep(2000);
+    	 }
+    	 action.click().perform();
+    	 
+    	action.sendKeys(Keys.TAB).build().perform();
+    	
+    	driver.findElement(By.xpath("//select[@id='year']")).click();
+    	
+    	for(int i=29; i>0; i--)
+    	{
+    		action.sendKeys(Keys.ARROW_DOWN).build().perform();
+    		
+    	}
+    	 action.click().perform();
+    	 
+    	 Thread.sleep(2000);
+    	 
+    	 WebElement radiobtn = driver.findElement(By.xpath("(//span[@class='_5k_2 _5dba'])[2]"));
+    	 action.sendKeys(radiobtn).click().perform();
+    	 
+    	 Thread.sleep(2000);
+    	 WebElement signupbtn = driver.findElement(By.xpath("(//button[text()='Sign Up'])[1]"));
+    	 signupbtn.click();
+    	 Thread.sleep(2000);
+    	 Assert.assertTrue(true, "Enter the confirmation code from the text message");
+    	 
+    	 System.out.println("Test get passed");
+    	 
+    	 
 	}
 
 }
